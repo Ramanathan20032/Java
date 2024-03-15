412ef3054d23090a03225de80088333493c4f053
Ramanathan20032 <154827095+Ramanathan20032@users.noreply.github.com>
2024-03-15 11:18:41 +0530

    Add files via upload

---
+++ Abstract_Classes_Methods/src/example/Shape.java
@@ -0,0 +1,36 @@
+package example;
+
+abstract public class Shape {
+    abstract int area(int a);
+}
+
+class Square extends Shape{
+    @Override
+    int area(int a) {
+        return a * a;
+    }
+}
+
+class Cube extends Shape{
+    @Override
+    int area(int a) {
+        return 6 * a * a;
+    }
+}
+
+class Abstract{
+    public static void main(String[] args) {
+        Square ob1 = new Square();
+        System.out.println(ob1.area(2));
+        Cube ob2 = new Cube();
+        System.out.println(ob2.area(2));
+
+        Shape[] shape = new Shape[2];
+        shape[0] = new Square();
+        shape[1] = new Cube();
+
+        for(Shape s : shape){
+            System.out.println(s.area(4));
+        }
+    }
+}
